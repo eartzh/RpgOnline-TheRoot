e5dd06482e9d864d3b7eb0f6a06ad5ae0d45c20f
eartzh <eartzh6563@gmail.com>
2023-06-05 21:05:54 +0800

    fixed overflow and gray page in github.io

--- script/scenes.js
+++ script/scenes.js
@@ -6,5 +6,5 @@ function home() {
   get("form").innerHTML='<object type="text/html" data="home.html"  width="100%" height="100%"></object>'
 }
 function gameStart(){
-  get("form").innerHTML='<object type="text/html" data="scene1.html"  width="100%" height="100%"></object>'
+  get("home").innerHTML='<object type="text/html" data="scene1.html"  width="100%" height="100%"></object>'
 }
