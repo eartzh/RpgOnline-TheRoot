7e57f545c8b7aa86849c0924fa3acfca6cdccc13
eartzh <eartzh6563@gmail.com>
2023-06-05 11:05:09 +0800

    added text player

--- script/env.js
+++ script/env.js
@@ -6,3 +6,23 @@ function get(id) {
 function getRnd(min,max) {
     return Math.floor(Math.random()*(max-min+1))+min;
 }
+function typing(){         
+    document.getElementById("text").innerHTML+=str.charAt(i);
+    i++;
+    var id=setTimeout(typing,80);
+    if(i==str.length){
+        clearTimeout(id);    
+    }
+}
+function onType(times){
+    t=0;
+    t1=setInterval(function(){
+        i=0;
+        document.getElementById("text").innerHTML=""
+        str=script[t]
+        console.log(t)
+        typing()
+        t=t+1
+        if(t>times) clearInterval(t1)
+    },4000)
+}
--- script/scene1.js
+++ script/scene1.js
@@ -1,12 +1,3 @@
 function remove(){
        
 }
-function typing(){         
-    var mydiv=document.getElementById("text");
-    mydiv.innerHTML+=str.charAt(i);
-    i++;
-    var id=setTimeout(typing,80);
-    if(i==str.length){
-        clearTimeout(id);    
-        }
-    }
