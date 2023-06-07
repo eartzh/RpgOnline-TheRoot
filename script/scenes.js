dbaee8b460f3111d323a844b606eef9ae971d887
eartzh <eartzh6563@gmail.com>
2023-06-07 12:40:11 +0800

    added Animation in home.html,gamestart log,organize directories

--- script/scenes.js
+++ script/scenes.js
@@ -6,5 +6,7 @@ function home() {
   get("form").innerHTML='<object type="text/html" data="home.html"  width="100%" height="100%"></object>'
 }
 function gameStart(){
-  get("home").innerHTML='<object type="text/html" data="scene1.html"  width="100%" height="100%"></object>'
+  get("home").classList.add('offLoad');
+  console.log("[debug]GameStart");
+  setTimeout(function(){get("home").innerHTML='<object type="text/html" data="scene1.html"  width="100%" height="100%"></object>'},1950)
 }
