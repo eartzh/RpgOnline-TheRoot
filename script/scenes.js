dbdf2a59d89cb2b1d7df747b2cc52d3290b5e7c4
eartzh <eartzh6563@gmail.com>
2023-06-07 12:14:45 +0800

    added Animation in home.html,gamestart log,organize directories

--- script/scenes.js
+++ script/scenes.js
@@ -1,10 +1,11 @@
 function splash() {
-    get("form").innerHTML='<object type="text/html" data="splash.html"  width="100%" height="100%"></object>'
+    get("form").innerHTML='<object type="text/html" data="/resource/scenes/splash.html"  width="100%" height="100%"></object>'
     setTimeout(function(){home();},3000)
 }
 function home() {
-  get("form").innerHTML='<object type="text/html" data="home.html"  width="100%" height="100%"></object>'
+  get("form").innerHTML='<object type="text/html" data="/resource/scenes/home.html"  width="100%" height="100%"></object>'
 }
 function gameStart(){
-  get("home").innerHTML='<object type="text/html" data="scene1.html"  width="100%" height="100%"></object>'
+  get("home").classList.add('offLoad');
+  setTimeout(function(){get("home").innerHTML='<object type="text/html" data="/resource/scenes/scene1.html"  width="100%" height="100%"></object>';},1950)
 }
