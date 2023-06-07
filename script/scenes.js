cb2ebe85734b45d017fc4fd2b5c1a80a5b163ae5
eartzh <eartzh6563@gmail.com>
2023-06-07 12:24:06 +0800

    Revert "fixed overflow and gray page in github.io"
    This reverts commit e5dd06482e9d864d3b7eb0f6a06ad5ae0d45c20f.

--- script/scenes.js
+++ script/scenes.js
@@ -1,11 +1,10 @@
 function splash() {
-    get("form").innerHTML='<object type="text/html" data="/resource/scenes/splash.html"  width="100%" height="100%"></object>'
+    get("form").innerHTML='<object type="text/html" data="splash.html"  width="100%" height="100%"></object>'
     setTimeout(function(){home();},3000)
 }
 function home() {
-  get("form").innerHTML='<object type="text/html" data="/resource/scenes/home.html"  width="100%" height="100%"></object>'
+  get("form").innerHTML='<object type="text/html" data="home.html"  width="100%" height="100%"></object>'
 }
 function gameStart(){
-  get("home").classList.add('offLoad');
-  setTimeout(function(){get("home").innerHTML='<object type="text/html" data="/resource/scenes/scene1.html"  width="100%" height="100%"></object>';},1950)
+  get("home").innerHTML='<object type="text/html" data="scene1.html"  width="100%" height="100%"></object>'
 }
