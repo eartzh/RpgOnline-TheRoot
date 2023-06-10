d8507e95715c3e26e6a5158ec82701d573795df6
eartzh <eartzh6563@gmail.com>
2023-06-10 23:40:48 +0800

    Revert 4cc20c5 and added scene2(map1,or 'djland')

--- script/ui.js
+++ script/ui.js
@@ -1,33 +1,44 @@
 function move(ev){
     ev.preventDefault();
-    if(ev.key=="w"&&char.y<=border.yMax){
+    if(ev.key=="w"&&char.y<border.yMax){
         char.y+=5
     }
-    if(ev.key=="a"&&char.x<=border.xMax){
+    if(ev.key=="a"&&char.x<border.xMax){
         char.x+=5
     }
-    if(ev.key=="s"&&char.y>=border.yMin){
+    if(ev.key=="s"&&char.y>border.yMin){
         char.y-=5
         if(char.y<=-300&&map=="tutorial"){   /* plugin for tutorial */
             scene2()
         }
     }
-    if(ev.key=="d"&&char.x>=border.xMin){
+    if(ev.key=="d"&&char.x>border.xMin){
         char.x-=5
     }
-    /*console.log(char.x,char.y)*/
+    console.log(char.x,char.y)
     get(map).style.left=char.x + "px"
     get(map).style.top=char.y + "px"
 }
 function scene2(){
+    map="djland"
     console.log("[debug]Enter map1")
+    get("tutorial").style.visibility="hidden"
+    get("djland").style.visibility="visible"
+    border.xMax=295
+    border.xMin=-800
+    border.yMax=350
+    border.yMin=-165
+    char.x=0
+    char.y=65
+    get("djland").style.left="0"
+    get("djland").style.top="65"
 }
 function tutorial(){
     get("box").classList.remove('box');
-    border.xMax=45
-    border.xMin=-35
-    border.yMax=425
-    border.yMin=-300
+    border.xMax=50
+    border.xMin=-40
+    border.yMax=430
+    border.yMin=-305
     map="tutorial"
     get("box").innerHTML="接受任務：前往碼頭"
     get("box").classList.add('box');
