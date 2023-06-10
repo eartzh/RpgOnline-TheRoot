319d4c9c0867cd4a99b43a5b9f55b6c098028443
eartzh <eartzh6563@gmail.com>
2023-06-10 16:37:35 +0800

    Revert "Revert "optimized codes in ui.html and ui.js""
    This reverts commit f33b74b703e5e1a14973b7d76a4b9bb21f0e384f.

--- script/ui.js
+++ script/ui.js
@@ -9,7 +9,7 @@ function move(ev){
     if(ev.key=="s"&&char.y>=border.yMin){
         char.y-=5
         if(char.y<=-300&&map=="tutorial"){   /* plugin for tutorial */
-            console.log("[debug]Enter map1")
+            scene2()
         }
     }
     if(ev.key=="d"&&char.x>=border.xMin){
@@ -18,4 +18,18 @@ function move(ev){
     /*console.log(char.x,char.y)*/
     get(map).style.left=char.x + "px"
     get(map).style.top=char.y + "px"
+}
+function scene2(){
+    console.log("[debug]Enter map1")
+}
+function tutorial(){
+    get("box").classList.remove('box');
+    border.xMax=45
+    border.xMin=-35
+    border.yMax=425
+    border.yMin=-300
+    map="tutorial"
+    get("box").innerHTML="接受任務：前往碼頭"
+    get("box").classList.add('box');
+    get("tutorial").style.visibility="visible"
 }
